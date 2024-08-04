c27b79eea6862497fb8da9a7f7416554e2544d05
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-08-04 17:40:08 -0500

    Created a function to log in console
    Added a javaScript funtion and it will eventually help us out with study material.

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,19 +1,23 @@
-const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript', 'Other'];
 
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopic() { 
+    if (topic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+    } else if (topic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+    } else if (topic === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+    } else if (topic === 'JavaScript') {
     console.log("Let's study JavaScript!");
-} else {
+    } else if (topic === 'Other') {
+    console.log("Let's study OTHER THINGS!");    
+    } else {
     console.log('Please try again!');
-}
-    */
+    }
+}   
